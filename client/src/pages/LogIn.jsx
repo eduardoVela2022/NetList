72fac827b73db2af0ef79a94737964675b0a7124
eduardoVela <eduardovela2022@icloud.com>
2024-09-28 14:28:51 -0600

    Added login functionality.

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,41 +1,51 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 // Log In page
 function LogIn() {
+  // Email state
   const [email, setEmail] = useState("tom@test.com");
+  // Password state
   const [password, setPassword] = useState("Pa$$w0rd");
 
+  const navigate = useNavigate();
+
+  // Handles the login form's submit event
   async function handleSubmit(event) {
     event.preventDefault();
 
+    // Creates an object with the user's information
     const userInfo = {
       email,
       password,
     };
 
-    const loginRes = await fetch(
-      "http://localhost:5000/api/login?useCookies=true",
-      {
-        method: "POST",
-        body: JSON.stringify(userInfo),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      }
-    );
+    // Login fetch request
+    await fetch("http://localhost:5000/api/login?useCookies=true", {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-    console.log(loginRes);
-  }
-
-  async function goToSignUp() {
+    // Auth fetch request
     const authRes = await fetch("http://localhost:5000/api/account", {
       credentials: "include",
     });
 
-    const dataAuth = await authRes.json();
+    // Converts auth fetch request to JSON
+    const authResData = await authRes.json();
 
-    console.log(dataAuth);
+    // Checks if user is authenticated (Cookie was created successfully)
+    if (authResData.isAuthenticated) {
+      // If so, go to homepage
+      navigate("homepage");
+    } else {
+      // Else, display error alert
+      alert("Incorrect log in credentials! Try again.");
+    }
   }
 
   // View
@@ -62,9 +72,9 @@ function LogIn() {
           Log in
         </button>
 
-        <button type="button" onClick={goToSignUp}>
-          Sign up instead
-        </button>
+        <Link to={"/sign-up"}>
+          <button>Sign up instead</button>
+        </Link>
       </form>
     </div>
   );
